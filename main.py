ad750b80943b1e2dcfbbb3841264fbfe6735cc6f
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2023-11-28 13:15:41 +0700

    Sua lai co che random b,l,q,s. Van con mot so loi o equation 1 2 3 4

--- main.py
+++ main.py
@@ -12,32 +12,37 @@ S1=np.array(S1)
 S2=np.array(S2)
 #x,y,z
 #gia preorder
+lrecord=np.array(np.random.randint(1,5,size=(8)))
+qrecord=lrecord+np.array(np.random.randint(10,20,size=(8)))
 
-A = np.random.randint(10, size=(8, 5))
+srecord=np.array(np.random.randint(3,5,size=(5)))
+
+brecord=srecord+np.array(np.random.randint(3,5,size=(5)))
+A = np.random.randint(1,10, size=(8, 5))
 A=np.array(A)
 
 i=Set(solver, name="i",records=["i" + str(x) for x in range (1,n+1)], description="Product index")
 j=Set(solver, name="j",records=["j" + str(x) for x in range (1,m+1)], description="Material index")
-b = Parameter(    container=solver, name="preorder_cost", domain=j, records=np.array([6,6,6,6,6]))
+b = Parameter(    container=solver, name="preorder_cost", domain=j, records=brecord)
 
-l = Parameter(    container=solver, name="cost_to_made_product", domain=i, records=np.array([5,5,5,5,5,5,5,5]))
+l = Parameter(    container=solver, name="cost_to_made_product", domain=i, records=lrecord)
 
-q = Parameter(    container=solver, name="revenue", domain=i, records=np.array([17,17,17,17,17,17,17,17]))
+q = Parameter(    container=solver, name="revenue", domain=i, records=qrecord)
 
-s= Parameter(   container=solver, name="revenue_when_sell_material", domain=j, records=np.array([4,4,4,4,4]))
+s= Parameter(   container=solver, name="revenue_when_sell_material", domain=j, records=srecord)
 
 A_1=Parameter(    solver,    name="Material_per_product",    domain=[i,j],    records=A)
 
 s1=Parameter(   solver,    name="Scenario1",    domain=i,    records=S1)
 
 s2=Parameter(    solver,    name="Scenario2",    domain=i,    records=S2)
-print(b.records)
-print(l.records)
-print(q.records)
-print(s.records)
-print(A_1.records)
-print(s1.records)
-print(s2.records)
+# print(b.records)
+# print(l.records)
+# print(q.records)
+# print(s.records)
+# print(A_1.records)
+# print(s1.records)
+# print(s2.records)
 
 x=Variable(    container=solver,    name="nguyen_lieu_order_truoc",    domain=j,    type="positive",    description="koqt")
 y1=Variable(    container=solver,    name="nguyen_lieu_du_th1",    domain=j,    type="positive",    description="koqt")
@@ -45,24 +50,25 @@ y2=Variable(    container=solver,    name="nguyen_lieu_du_th2",    domain=j,    
 z1=Variable(    container=solver,    name="san_pham_san_xuat_th1",    domain=i,    type="positive",    description="koqt")
 z2=Variable(    container=solver,    name="san_pham_san_xuat_th2",    domain=i,    type="positive",    description="koqt")
 
-e1=Equation(    solver,    name="e1",    domain=[i],    definition=z1[i]<=s1[i])
-e2=Equation(    solver,    name="e2",    domain=[i],    definition=z2[i]<=s1[i])
+e1=Equation(    solver,    name="e1",    domain=[i])
+e1[i]=z1[i]<=s1[i]
+e2=Equation(    solver,    name="e2",    domain=[i])
+e2[i]=z2[i]<=s2[i]
+e3=Equation(    solver,    name="e3",   domain=[j])
+e3[j]=y1[j]==x[j]-A_1*z1
+e4=Equation(    solver,    name="e4",domain=[j])
+e4[j]=y2[j]==x[j]-A_1*z2
 
-e3=Equation(    solver,    name="e3",    domain=[i,j],   definition=y1 == x-A_1*z1)
-e4=Equation(    solver,    name="e4",    domain=[i,j],    definition=y2 == x-A_1*z2)
 
-print(b.records)
-print(l.records)
-print(q.records)
-print(s.records)
 
 result=Model(
     solver,
-    equations=[e1,e2,e3,e4],
+    
+    name="solver",
+    equations=solver.getEquations(),
     problem="LP",
     sense=Sense.MIN ,
-    objective=b*x+1/2*((l-q)*z1-s*y1)+1/2*((l-q)*z2-s*y2),
-    name="solver"
+    objective=b*x+1/2*((l-q)*z1-s*y1)+1/2*((l-q)*z2-s*y2)
 )
 import sys
 result.solve()
