6b6f3cb2f12158b9faf30ff7fe9ce6e06de18c5d
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2023-11-28 15:32:11 +0700

    Sua lai co che random b,l,q,s. Van con mot so loi o equation 1 2 3 4 ..

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ qrecord=lrecord+np.array(np.random.randint(10,20,size=(8)))
 srecord=np.array(np.random.randint(3,5,size=(5)))
 
 brecord=srecord+np.array(np.random.randint(3,5,size=(5)))
-A = np.random.randint(1,10, size=(8, 5))
-A=np.array(A)
+
 
 i=Set(solver, name="i",records=["i" + str(x) for x in range (1,n+1)], description="Product index")
 j=Set(solver, name="j",records=["j" + str(x) for x in range (1,m+1)], description="Material index")
+
 b = Parameter(    container=solver, name="preorder_cost", domain=j, records=brecord)
 
 l = Parameter(    container=solver, name="cost_to_made_product", domain=i, records=lrecord)
@@ -31,31 +31,38 @@ q = Parameter(    container=solver, name="revenue", domain=i, records=qrecord)
 
 s= Parameter(   container=solver, name="revenue_when_sell_material", domain=j, records=srecord)
 
+A = np.random.randint(1,10, size=(8,5))
+A=np.array(A)
 A_1=Parameter(    solver,    name="Material_per_product",    domain=[i,j],    records=A)
+
 
 s1=Parameter(   solver,    name="Scenario1",    domain=i,    records=S1)
 
 s2=Parameter(    solver,    name="Scenario2",    domain=i,    records=S2)
-# print(b.records)
-# print(l.records)
-# print(q.records)
-# print(s.records)
-# print(A_1.records)
-# print(s1.records)
-# print(s2.records)
+print(b.records)
+print(l.records)
+print(q.records)
+print(s.records)
+print(A_1.records)
+print(s1.records)
+print(s2.records)
 
-x=Variable(    container=solver,    name="nguyen_lieu_order_truoc",    domain=j,    type="positive",    description="koqt")
-y1=Variable(    container=solver,    name="nguyen_lieu_du_th1",    domain=j,    type="positive",    description="koqt")
-y2=Variable(    container=solver,    name="nguyen_lieu_du_th2",    domain=j,    type="positive",    description="koqt")
-z1=Variable(    container=solver,    name="san_pham_san_xuat_th1",    domain=i,    type="positive",    description="koqt")
-z2=Variable(    container=solver,    name="san_pham_san_xuat_th2",    domain=i,    type="positive",    description="koqt")
+x=Variable(    container=solver,    name="nguyen_lieu_order_truoc",    domain=j,    type="positive")
+y1=Variable(    container=solver,    name="nguyen_lieu_du_th1",    domain=j,    type="positive")
+y2=Variable(    container=solver,    name="nguyen_lieu_du_th2",    domain=j,    type="positive")
+z1=Variable(    container=solver,    name="san_pham_san_xuat_th1",    domain=i,    type="positive")
+z2=Variable(    container=solver,    name="san_pham_san_xuat_th2",    domain=i,    type="positive")
+
 
 e1=Equation(    solver,    name="e1",    domain=[i])
 e1[i]=z1[i]<=s1[i]
+
 e2=Equation(    solver,    name="e2",    domain=[i])
 e2[i]=z2[i]<=s2[i]
+
 e3=Equation(    solver,    name="e3",   domain=[j])
 e3[j]=y1[j]==x[j]-A_1*z1
+
 e4=Equation(    solver,    name="e4",domain=[j])
 e4[j]=y2[j]==x[j]-A_1*z2
 
