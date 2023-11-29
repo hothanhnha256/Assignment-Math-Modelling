11392914349f2115fc26a9aa7d5b76262adb87bf
Nguyen Huy Hoang <hoang.nguyen2211093@hcmut.edu.vn>
2023-11-29 23:13:50 +0700

    11pm 29/11

--- hoang.py
+++ hoang.py
@@ -9,12 +9,14 @@ print("---------------Start World!-----------------\n")
     #*             x = pre-order_material, y = inventory, z = production
     #* Constraints:
     #*             i,j > 0
+    #*             aij >=0
     #*             sj < bj
     #*             0 <= z <= d
     #*             y >= 0
-    #*             ci = li - qi
-    #*             yj = xj - SumXichMa(aij.zi)
-    #*             Objective Function: min Z(z,y) = SumXichMa(li - qi)zi + SumXichMa(sj.yj)
+    #*             y = x - A.T*z
+    #*             Objective Function: 
+    #todo                          (7)  min Z(z,y) = c.T*z - s.T*y
+    #todo                          (8)  min g(x,zT,yT) = b.T*x - E(Z(z))
 
 # import pandas as pd
 import numpy as np  
@@ -32,103 +34,91 @@ j=Set(model, name="j", description="materials", records=["j" + str(x) for x in r
 b=Parameter(
     model, name="b", description="pre-order_cost",
     domain=j,
-    records= np.random.randint(1, 100, size=(1,m)),
+    records= np.random.randint(51, 100, size=(1,m)),
 )
 s=Parameter(
     model, name="s", description="inventory_cost",
     domain=j,
-    records= np.random.randint(1, 100, size=(1,m)),
+    records= np.random.randint(1, 50, size=(1,m)),
 )
 l=Parameter(
     model, name="l", description="production_cost",
     domain=i,
-    records= np.random.randint(1, 100, size=(1,n)),
+    records= np.random.randint(1, 50, size=(1,n)),
 )
 q=Parameter(
-    model, name="q", description="sale",
-    domain=i, 
-    records= np.random.randint(1, 100, size=(1,n)),
+    model, name="q", description="price product",
+    domain=i,
+    records= np.random.randint(151, 1000, size=(1,n)),
 )
 c=Parameter(
-    model, name="c", domain=i,
+    model, name="c", description="equation c= l-q",
+    domain=i,
 )
-c[i] = l[i] - q[i]
+c[i] = l[i]-q[i]
 
 ps=Parameter(
     model, name="ps", description="probability of scenario",
-    domain= sc,
+    domain=sc,
+    records=np.array([0.5, 0.5]), # ps[sc]= 0.5
 )
-ps[sc]= 0.5
-
 d=Parameter(
-    model,  name="d",
+    model, name="d", description="demand",
     domain=[sc,i],
     records= np.random.binomial(10, 0.5, size=(scenario,n)),
 )
 a=Parameter(
     container=model, name="a",
-    domain=[i, j],
+    domain=[i,j],
     # matrix A(n x m)
     description="aij",
-    records= np.random.randint(10, size=(8, 5))
+    records= np.random.randint(10, size=(8, 5)),
 )
 
 x=Variable(
     model, name="x",
-    domain= j,
+    domain=j,
     type="positive", description="Sum pre-order_material",
 )
 y=Variable(
     model, name="y",
-    domain= j,
-    type="positive", description="inventory"
+    domain=[sc,j],
+    type="positive", description="inventory",
 )
 z=Variable(
     model, name="z",
-    domain= i,
-    type="positive", description="production"
+    domain=[sc,i],
+    type="positive", description="production",
 )
 
-supply1 = Equation(
-    model, name="supply1", domain=j,
-)
+# a[i,j] >= 0 đã được thể hiện trong parameter a
+# s[j] < b[j] đã được thể hiện trong parameter s và b
+#* VÀ VÌ chỉ toàn tham số (hàm ko có biến)
+
+supply1 = Equation(model, name="supply1", domain=j,)
 supply1[j] = x[j] >= 0
 
-supply2 = Equation(
-    model,  name="supply2", domain=j, 
-)
-# y[j] = x[j] - Sum(i, a[i,j]*z[i]) >= 0
-supply2[j] = x[j] - Sum(i, a[i,j]*z[i]) >= 0
+supply2 = Equation(model,  name="supply2", domain=[sc, j],)
+supply2[sc, j]= y[sc,j] == x[j] - Sum(i, a[i,j]*z[sc, i])
 
-supply3 = Equation(
-    model,  name="supply3",  domain=[i,j], 
-)
-supply3[i,j] = a[i,j] >= 0
+supply3 = Equation(model, name="supply3", domain=[sc,j],)
+supply3[sc,j] = y[sc,j] >= 0
 
-supply4 = Equation(
-    model,  name="supply4",  domain=j, 
-)
-supply4[j] = s[j] < b[j]
+demand1 = Equation(model, name="demand1",  domain=[sc,i],)
+demand1[sc,i] = z[sc,i] <= d[sc,i]
 
-demand1 = Equation(
-    model, name="demand1",  domain=i, 
-)
-demand1[i] = z[i] <= d[i]
-
-demand2 = Equation(
-    model, name="demand2", domain=i, 
-)
-demand2[i] = z[i] >= 0
+demand2 = Equation(model, name="demand2", domain=[sc,i],)
+demand2[sc,i] = z[sc,i] >= 0
 
 #! equation 7+8
 equation78 = Model(
     model, name="equation78",
-    equations=[supply1, supply2, supply3, supply4, demand1, demand2],
+     equations=[supply1, supply2, supply3, demand1, demand2],
     problem="LP",
     #tính min
     sense=Sense.MIN,
     #hàm mục tiêu
-    objective= Sum(j, b[j]*x[j]) - Sum(sc, ps[sc]*(Sum(i, c[i]*z[i]) + Sum(j, s[j]*(x[j] - Sum(i, a[i,j]*z[i]))))),
+    objective= Sum(j, b[j]*x[j]) + Sum(sc, ps[sc]*(Sum(i, c[i]*z[sc,i]) - Sum(j, s[j]*y[sc,j]))),
 )
 
 #  solve
