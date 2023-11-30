d89bf6266ed58f3d3ecdf48b92e3e75974912f97
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2023-11-30 16:06:51 +0700

    Sua loi khong giai thanh so nguyen, done BTL

--- main.py
+++ main.py
@@ -1,82 +1,135 @@
-import numpy as np
-import numpy.matlib 
-import pandas as pd
+print("---------------Start Program!-----------------\n")
+#! convert the sample problem into BTL
+    #* Indices (Sets):
+    #*             i = product, j = material
+    #* Parameters:
+    #*             b = pre-order_cost, l = production_cost, q = sale, s = inventory_cost, d = demand, c= l-q , a = maxtrix [i,j]
+    #* Decision variables:
+    #*             x = pre-order_material, y = inventory, z = production
+    #* Constraints:
+    #*             i,j > 0
+    #*             aij >=0
+    #*             sj < bj
+    #*             0 <= z <= d
+    #*             y >= 0
+    #*             y = x - A.T*z
+    #*             Objective Function: 
+    #todo                          (7)  min Z(z,y) = c.T*z - s.T*y
+    #todo                          (8)  min g(x,zT,yT) = b.T*x - E(Z(z))
+
+# import pandas as pd
+import numpy as np  
 from gamspy import Container, Set, Parameter, Variable, Equation, Model, Sum, Sense
-solver=Container()
-n=8 #loai san pham
-m=5 #loai nguyen lieu
-S1=np.random.binomial(10,.5,n)
-S2=np.random.binomial(10,.5,n) 
-S=0.5 #senario
-S1=np.array(S1)
-S2=np.array(S2)
-#x,y,z
-#gia preorder
-lrecord=np.array(np.random.randint(1,5,size=(8)))
-qrecord=lrecord+np.array(np.random.randint(10,20,size=(8)))
 
-srecord=np.array(np.random.randint(3,5,size=(5)))
+model=Container()
+n=8 # i type product
+m=5 # j type material
+scenario = 2 # 2 scenario
 
-brecord=srecord+np.array(np.random.randint(3,5,size=(5)))
+sc=Set(model, name ="sc", description="scenario", records= ["sc"+ str(k) for k in range (1, scenario + 1)])
+i=Set(model, name="i", description="productions", records=["i" + str(x) for x in range (1,n+1)])
+j=Set(model, name="j", description="materials", records=["j" + str(x) for x in range (1,m+1)])
 
-
-i=Set(solver, name="i",records=["i" + str(x) for x in range (1,n+1)], description="Product index")
-j=Set(solver, name="j",records=["j" + str(x) for x in range (1,m+1)], description="Material index")
-
-b = Parameter(    container=solver, name="preorder_cost", domain=j, records=brecord)
-
-l = Parameter(    container=solver, name="cost_to_made_product", domain=i, records=lrecord)
-
-q = Parameter(    container=solver, name="revenue", domain=i, records=qrecord)
-
-s= Parameter(   container=solver, name="revenue_when_sell_material", domain=j, records=srecord)
-
-A = np.random.randint(1,10, size=(8,5))
-A=np.array(A)
-A_1=Parameter(    solver,    name="Material_per_product",    domain=[i,j],    records=A)
-
-
-s1=Parameter(   solver,    name="Scenario1",    domain=i,    records=S1)
-
-s2=Parameter(    solver,    name="Scenario2",    domain=i,    records=S2)
-print(b.records)
-print(l.records)
-print(q.records)
-print(s.records)
-print(A_1.records)
-print(s1.records)
-print(s2.records)
-
-x=Variable(    container=solver,    name="nguyen_lieu_order_truoc",    domain=j,    type="positive")
-y1=Variable(    container=solver,    name="nguyen_lieu_du_th1",    domain=j,    type="positive")
-y2=Variable(    container=solver,    name="nguyen_lieu_du_th2",    domain=j,    type="positive")
-z1=Variable(    container=solver,    name="san_pham_san_xuat_th1",    domain=i,    type="positive")
-z2=Variable(    container=solver,    name="san_pham_san_xuat_th2",    domain=i,    type="positive")
-
-
-e1=Equation(    solver,    name="e1",    domain=[i])
-e1[i]=z1[i]<=s1[i]
-
-e2=Equation(    solver,    name="e2",    domain=[i])
-e2[i]=z2[i]<=s2[i]
-
-e3=Equation(    solver,    name="e3",   domain=[j])
-e3[j]=y1[j]==x[j]-A_1*z1
-
-e4=Equation(    solver,    name="e4",domain=[j])
-e4[j]=y2[j]==x[j]-A_1*z2
-
-
-
-result=Model(
-    solver,
-    
-    name="solver",
-    equations=solver.getEquations(),
-    problem="LP",
-    sense=Sense.MIN ,
-    objective=b*x+1/2*((l-q)*z1-s*y1)+1/2*((l-q)*z2-s*y2)
+b=Parameter(
+    model, name="b", description="pre-order_cost",
+    domain=j,
+    records= np.random.randint(100, 500, size=(1,m)),
 )
+s=Parameter(
+    model, name="s", description="inventory_cost",
+    domain=j,
+    records= np.random.randint(1, 99, size=(1,m)),
+)
+l=Parameter(
+    model, name="l", description="production_cost",
+    domain=i,
+    records= np.random.randint(100, 500, size=(1,n)),
+)
+q=Parameter(
+    model, name="q", description="price product",
+    domain=i,
+    records= np.random.randint(1000, 10000, size=(1,n)),
+)
+c=Parameter(
+    model, name="c", description="equation c= l-q",
+    domain=i,
+)
+c[i] = l[i]-q[i]
+
+ps=Parameter(
+    model, name="ps", description="probability of scenario",
+    domain=sc,
+    records=np.array([0.5, 0.5]), # ps[sc]= 0.5
+)
+d=Parameter(
+    model, name="d", description="demand",
+    domain=[sc,i],
+    records= np.random.binomial(10, 0.5, size=(scenario,n)),
+)
+a=Parameter(
+    container=model, name="a",
+    domain=[i,j],
+    # matrix A(n x m)
+    description="aij",
+    records= np.random.randint(1,10, size=(8, 5)),
+)
+
+x=Variable(
+    model, name="x",
+    domain=j,
+    type="integer", description="Sum pre-order_material",
+)
+y=Variable(
+    model, name="y",
+    domain=[sc,j],
+    type="integer", description="inventory",
+)
+z=Variable(
+    model, name="z",
+    domain=[sc,i],
+    type="integer", description="production",
+)
+
+# a[i,j] >= 0 đã được thể hiện trong parameter a
+# s[j] < b[j] đã được thể hiện trong parameter s và b
+#* VÀ VÌ chỉ toàn tham số (hàm ko có biến)
+
+supply1 = Equation(model, name="supply1", domain=j,)
+supply1[j] = x[j] >= 0
+
+supply2 = Equation(model,  name="supply2", domain=[sc, j],)
+supply2[sc, j]= y[sc,j] == x[j] - Sum(i, a[i,j]*z[sc, i])
+
+supply3 = Equation(model, name="supply3", domain=[sc,j],)
+supply3[sc,j] = y[sc,j] >= 0
+
+demand1 = Equation(model, name="demand1",  domain=[sc,i],)
+demand1[sc,i] = z[sc,i] <= d[sc,i]
+
+demand2 = Equation(model, name="demand2", domain=[sc,i],)
+demand2[sc,i] = z[sc,i] >= 0
+
+#! equation 7+8
+equation78 = Model(
+    model, name="equation78",
+    equations=[supply1, supply2, supply3, demand1, demand2],
+    problem="MIP",
+    #tính min
+    sense=Sense.MIN,
+    #hàm mục tiêu
+    objective= Sum(j, b[j]*x[j]) + Sum(sc, ps[sc]*(Sum(i, c[i]*z[sc,i]) - Sum(j, s[j]*y[sc,j]))),
+)
+
+#  solve : giải optimal
 import sys
-result.solve()
+#in ra toàn bộ chương trình
+    # equation78.solve(output=sys.stdout)
+equation78.solve()
+print("\nThe solution X of the equation is: ")
 print(x.records)
+print("\nThe solution Y of the equation is: ")
+print(y.records)
+print("\nThe solution Z of the equation is: ")
+print(z.records)
+print(f'Optimal solution found = {equation78.objective_value}')
+print("\n----------------End program----------------")
